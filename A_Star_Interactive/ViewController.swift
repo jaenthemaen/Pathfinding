5e7ca52b27087b2db98eb4c0c307403b728a8276
Jan Mueller <jan.mueller@datagroup.de>
2019-03-02 22:20:50 +0100

    Initial Commit

---
+++ A_Star_Interactive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A_Star_Interactive
+//
+//  Created by Jan Müller on 02.03.19.
+//  Copyright © 2019 Jan Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
